d5817d8eff7fd5bc7877ad90f9112797e0dff230
JuanGambetta <88690180+JuanGambetta@users.noreply.github.com>
2021-10-20 14:14:05 -0300

    commit inicial/final

---
+++ PipesFilters/src/Program/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CompAndDel.Pipes;
+using CompAndDel.Filters;
+
+namespace CompAndDel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+            IPipe nulo = new PipeNull();
+            IFilter negativo = new FilterNegative();
+            IPipe serial1 = new PipeSerial(negativo,nulo);
+            IFilter gris = new FilterGreyscale();
+            IPipe serial2 = new PipeSerial(gris, serial1);
+            IPicture final = serial2.Send(picture);
+            provider.SavePicture(final, @"save.jpg");
+        }
+    }
+}
